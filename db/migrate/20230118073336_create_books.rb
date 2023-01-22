ee24cc8740325a6ac01dec0f3977fc0b109c419a
EC2 Default User <ec2-user@ip-172-31-40-46.ap-northeast-1.compute.internal>
2023-01-22 09:49:47 +0000

    [Error]エラー文発生中

--- db/migrate/20230118073336_create_books.rb
+++ db/migrate/20230118073336_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-      t.string :name
-      t.string :introduction
+      t.string :title
+      t.string :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -41,8 +41,8 @@ ActiveRecord::Schema.define(version: 2023_01_18_073336) do
   end
 
   create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.string "introduction"
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2023_01_18_073336) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
